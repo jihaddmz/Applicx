cfb36e081d1a7d1e12743cb928c4415b9c080f3f
AppsFourLife <developer@appsfourlife.com>
2023-12-01 20:03:28 +0200

    Finished initial Simple bottomnavbar

--- lib/screens/screen_home.dart
+++ lib/screens/screen_home.dart
@@ -1,5 +1,6 @@
 import 'package:applicx/components/text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class ScreenHome extends StatefulWidget {
   @override
@@ -7,9 +8,61 @@ class ScreenHome extends StatefulWidget {
 }
 
 class _ScreenHome extends State<ScreenHome> {
+  int _selectedIndex = 0;
+  String _iconHome = "assets/svgs/vector_home_black.svg";
+  String _iconReports = "assets/svgs/vector_reports.svg";
+  String _iconSettings = "assets/svgs/vector_settings.svg";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        selectedFontSize: 15,
+        unselectedFontSize: 10,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              _iconHome,
+              semanticsLabel: "Home",
+            ),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              _iconReports,
+              semanticsLabel: "Reports",
+            ),
+            label: 'Reports',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              _iconSettings,
+              semanticsLabel: "Settings",
+            ),
+            label: 'Settings',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: const Color(0xff243141),
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+          if (index == 0) {
+            _iconHome = "assets/svgs/vector_home_black.svg";
+            _iconReports = "assets/svgs/vector_reports.svg";
+            _iconSettings = "assets/svgs/vector_settings.svg";
+          } else if (index == 1) {
+            _iconHome = "assets/svgs/vector_home.svg";
+            _iconReports = "assets/svgs/vector_reports_black.svg";
+            _iconSettings = "assets/svgs/vector_settings.svg";
+          } else {
+            _iconHome = "assets/svgs/vector_home.svg";
+            _iconReports = "assets/svgs/vector_reports.svg";
+            _iconSettings = "assets/svgs/vector_settings_black.svg";
+          }
+        },
+      ),
       body: Center(
         child: TextBoldBlack("Introducing APPLICX"),
       ),
